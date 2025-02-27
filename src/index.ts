5ef744adeeddbdb332e545f7adaff5029a09fbc7
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-28 00:05:22 +0600

    insert and retrive data from database

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+
+const main = async () => {
+  //   const result = await prisma.post.create({
+  //     data: {
+  //       title: 'This is title 2',
+  //       content: 'This is content 2',
+  //       authorName: 'Yeasin Rafee',
+  //     },
+  //   });
+  //   console.log(result);
+
+  const getAllFromDB = await prisma.post.findMany();
+  console.log(getAllFromDB);
+};
+
+main();
