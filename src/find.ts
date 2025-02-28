3f6fc09c8d24f3e650a062b090ea95c2d00c65be
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-03-01 00:10:50 +0600

    use the select for selected fields

--- src/find.ts
+++ src/find.ts
@@ -4,22 +4,27 @@ const prisma = new PrismaClient();
 
 const main = async () => {
   // find all
-  const getAllFromDB = await prisma.post.findMany();
+  // const getAllFromDB = await prisma.post.findMany();
 
   // find first and find first or throw error
-  const findFirst = await prisma.post.findFirstOrThrow({
-    where: {
-      id: 1,
-    },
-  });
+  // const findFirst = await prisma.post.findFirstOrThrow({
+  //   where: {
+  //     id: 1,
+  //   },
+  // });
 
   // find unique and find unique or throw error
   const findUnique = await prisma.post.findUniqueOrThrow({
     where: {
-      id: 2,
+      id: 6,
+    },
+    select: {
+      title: true,
+      content: true,
+      authorName: true,
     },
   });
-  console.log(findUnique);
+  console.log({ findUnique });
 };
 
 main();
