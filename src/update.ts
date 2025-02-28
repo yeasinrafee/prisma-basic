a35b2d1057afe7bc469649e55769998de1be5fee
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-28 23:47:58 +0600

    Update Many

--- src/update.ts
+++ src/update.ts
@@ -3,18 +3,27 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const updates = async () => {
-  const singleUpdate = await prisma.post.update({
+  //   const singleUpdate = await prisma.post.update({
+  //     where: {
+  //       id: 3,
+  //     },
+  //     data: {
+  //       title: 'This is title 3',
+  //       content: 'This is content 3',
+  //       authorName: 'Yeasin Rafee',
+  //     },
+  //   });
+
+  const updateMany = await prisma.post.updateMany({
     where: {
-      id: 3,
+      title: 'Title 2',
     },
     data: {
-      title: 'This is title 3',
-      content: 'This is content 3',
       authorName: 'Yeasin Rafee',
     },
   });
 
-  console.log(singleUpdate);
+  console.log(updateMany);
 };
 
 updates();
