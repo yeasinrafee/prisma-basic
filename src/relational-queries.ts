7ffc60322cbd772afa697579805da1bb53ce4ed2
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-03-02 12:17:37 +0600

    Relation filters

--- src/relational-queries.ts
+++ src/relational-queries.ts
@@ -3,16 +3,27 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const relationalQueries = async () => {
-  const result = await prisma.user.findUnique({
-    where: {
-      id: 2,
-    },
+  //   const result = await prisma.user.findUnique({
+  //     where: {
+  //       id: 2,
+  //     },
+  //     include: {
+  //       post: true,
+  //     },
+  //   });
+
+  // relational filters
+  const publishedPostUser = await prisma.user.findMany({
     include: {
-      post: true,
+      post: {
+        where: {
+          published: true,
+        },
+      },
     },
   });
 
-  console.log(result);
+  console.log(publishedPostUser);
 };
 
 relationalQueries();
