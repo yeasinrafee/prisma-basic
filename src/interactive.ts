339f84e0c13bcb07fea4cc4ab40afe184be600a0
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-03-03 16:54:23 +0600

    Interactive Transaction

---
+++ src/interactive.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const interactiveTransaction = async () => {
+  const result = await prisma.$transaction(async (transactionClient) => {
+    // query: 1
+    const getAllPost = await transactionClient.post.findMany({
+      where: {
+        published: true,
+      },
+    });
+
+    // query: 2
+    const countUser = await transactionClient.user.count();
+
+    // query: 3
+    const updateUser = await transactionClient.user.update({
+      where: {
+        id: 6,
+      },
+      data: {
+        age: 80,
+      },
+    });
+
+    return {
+      getAllPost,
+      countUser,
+      updateUser,
+    };
+  });
+  console.log(result);
+};
+
+interactiveTransaction();
